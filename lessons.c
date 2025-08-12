910bd33c62a9b9264e3a89a10b85a851f60b2f4f
MIHAIL ZAGORODNUYK <mikle.zag@gmail.com>
2025-08-12 23:52:05 +0300

    добавлена возможность записывать уравнения

--- lessons.c
+++ lessons.c
@@ -4,6 +4,8 @@
 #include <math.h>
 #include <ctype.h>
 #define MAXSIZE 100
+int flag = 1; //принимает значение -1 для чисел, находящихся в правой части ур-я
+
 
 float koefficient(char dgt_massive[], int l, int r);
 
@@ -16,7 +18,7 @@ int main() {
     int l, size;
     l = size = 0;
     a = b = c = 0;
-    // Введите квадратное уравнение вида ax^2+bx+c
+    // Введите квадратное уравнение
     while ((symb = getchar()) != '\n' && size < MAXSIZE - 1) {
         massive[size++] = symb;
     }
@@ -24,24 +26,24 @@ int main() {
     for (int r = 0; r < size; r++) {
         if (massive[r] == 'x') {
             switch (massive[r + 1]) {
-                    case '^': { //если стоит знак степени, значит это коэфф a
-                        a = koefficient(massive, l, r - 1);
-                        r += 3;
-                        l = r;
-                        break;
-                    }
-                    default: { //степени нет, значит коэфф b
-                        b = koefficient(massive, l, r - 1);
-                        r += 1;
-                        l = r;
-                        break;
-                    }
+                case '^': { //если стоит знак степени, значит это коэфф a
+                    a += koefficient(massive, l, r - 1);
+                    r += 3;
+                    l = r;
+                    break;
+                }
+                default: { //степени нет, значит коэфф b
+                    b += koefficient(massive, l, r - 1);
+                    r += 1;
+                    l = r;
+                    break;
+                }
             }
         }
         else
             switch(massive[r + 1]){
-                case '+': case '-': case '\0': {
-                    c = koefficient(massive, l, r);
+                case '+': case '-': case '\0': case '=': {
+                    c += koefficient(massive, l, r);
                     r += 1;
                     l = r;
                     break;
@@ -49,6 +51,10 @@ int main() {
                 default:
                     break;
             }
+        if (massive[l] == '=') {
+            flag = -1;
+            l++;
+        }
     }
 
     if (a) {
@@ -62,33 +68,39 @@ int main() {
         }
         else if (discr == 0) {
             x1 = -b / (2 * a);
-            printf("Корень: %.2f \n", x1);
+            printf("Корень: %.2f\n", x1);
         }
         else
-            printf("Корни есть, но они мнимые(");
+            printf("Корни есть, но они мнимые(\n");
     }
     else {
         if (b) {
             x1 = -c / b;
             printf("Корень: %.2f\n", x1);
         }
-        else
-        printf("Корней нет(");
+        else {
+            if (c)
+                printf("Корней нет(\n");
+            else
+                printf("Корень: 0\n");
+        }
     }
+    printf("Коэффициенты приведенного уравнения:\n");
+    printf("x^2: %.2f, x^1: %.2f, x^0: %.2f\n", a, b, c);
     return 0;
 }
 
 float koefficient(char dgt_massive[], int l, int r) { //функция получения численного коэффициента из подстроки
-    if (r + 1 == l) { //в случае, когда перед х-ом нет коэффициента
+    if (r <= l) { //в случае, когда перед х-ом нет коэффициента
         if (isdigit(dgt_massive[l])) //проверка, что это коэфф перед х, а не цифра
-            return dgt_massive[l] - '0';
+            return (dgt_massive[l] - '0') * flag;
         else
-            return 1;
+            return flag;
     }
     char digits[r - l + 1];
     float nums;
     for (int i = l; i <= r; i++)
         digits[i - l] = dgt_massive[i];
     nums = atof(digits);
-    return nums;
+    return nums * flag;
 }
